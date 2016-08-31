abc4d05459fa91fe32b57993a38becffe1ae3b5b
malaghope <chocoultrachoc@yahoo.fr>
2016-08-31 10:55:06 +0200

    adazda

---
+++ SpringBanque-Business/src/main/java/fr/afcepf/al28/seb/banque/business/api/IBusinessClient.java
@@ -0,0 +1,11 @@
+package fr.afcepf.al28.seb.banque.business.api;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IBusinessClient {
+//    List<Compte> afficherComptes();
+//    List<Operation> afficherComptes();
+}
---
+++ SpringBanque-Business/src/main/java/fr/afcepf/al28/seb/banque/business/api/IBusinessConseiller.java
@@ -0,0 +1,8 @@
+package fr.afcepf.al28.seb.banque.business.api;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IBusinessConseiller {
+
+}
